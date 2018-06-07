11501f648184311d6afb5062d59b8e73d6eb4e78
kanekotic <alvarojosepl@gmail.com>
2018-06-07 09:32:19 +0200

    serve github page

--- example/web/main.js
+++
@@ -1,4 +0,0 @@
-const convert = require('../../lib/index.js').convertRaw
-
-var button = document.getElementById('convert')
-button.addEventListener('click', ()=> document.getElementById('stringGuid').value = convert(document.getElementById('rawGuid').value))
