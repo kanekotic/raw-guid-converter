31e2d80e054330f28131c2ce39d8def7d6282cc6
kanekotic <alvarojosepl@gmail.com>
2018-06-08 17:24:44 +0200

    added error handling for command line exeptions

--- bin/raw-to-guid.js
+++ bin/raw-to-guid.js
@@ -12,14 +12,26 @@ program
   .command('fromRaw [guid...]')
   .description('converts raw guid to string format')
   .action((guids) => 
-  guids.forEach(guid => console.log(index.convertRaw(guid)))
+  guids.forEach(guid => {
+    try {
+      console.log(index.convertRaw(guid))
+    } catch (_) {
+      console.log(`${guid}: is not a valid raw format for a guid`)
+    }
+  })
 )
 
 program
   .command('fromString [guid...]')
   .description('converts string guid to raw format')
   .action((guids) => 
-  guids.forEach(guid => console.log(index.convertString(guid)))
-)
+    guids.forEach(guid => {
+      try {
+        console.log(index.convertString(guid))
+      } catch (_) {
+        console.log(`${guid}: is not a valid string format for a guid`)
+      }
+    }
+  ))
   
   program.parse(process.argv)
