ccd6099e50941aca71db869670fb227b3a4a2530
kanekotic <alvarojosepl@gmail.com>
2018-06-08 16:56:22 +0200

    added convertGuid to raw to export

--- test/lib/index-test.js
+++ test/lib/index-test.js
@@ -1,11 +1,16 @@
 jest.mock('../../lib/raw-to-guid', () => jest.fn())
+jest.mock('../../lib/guid-to-raw', () => jest.fn())
 
 const index = require('../../lib/index'),
-    convertRaw = require('../../lib/raw-to-guid')
+    convertRaw = require('../../lib/raw-to-guid'),
+    convertGuid = require('../../lib/guid-to-raw')
 
     
 describe('index should export functions', () => {
-    it('calls raw to guid in rawConver', () => {
+    it('calls raw to guid in rawConvert', () => {
         expect(index.convertRaw).toEqual(convertRaw)
+    })
+    it('calls guid to raw in guidConvert', () => {
+        expect(index.convertGuid).toEqual(convertGuid)
     })
 })
