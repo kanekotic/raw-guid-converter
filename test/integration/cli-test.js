7947ac835f0b35c75a2066f5d3f309fd1c34db58
kanekotic <alvarojosepl@gmail.com>
2018-07-09 08:57:53 +0200

    added integration test

---
+++ test/integration/cli-test.js
@@ -0,0 +1,59 @@
+const coffee = require('coffee'),
+    path = require('path'),
+    pathToExecutable = path.join(__dirname, '../../bin/raw-to-guid.js')
+    
+describe('cli', () => {
+
+    [
+        ["4630880E6D0B3640AB446C6FB3C44FE3","0E883046-0B6D-4036-AB44-6C6FB3C44FE3\n".toLowerCase()],
+        ["6C6F59B2D4C5144AB7786A304CCB597F","B2596F6C-C5D4-4A14-B778-6A304CCB597F\n".toLowerCase()],
+    ].forEach(guid => {
+        it(`should convert ${guid[1]} string into ${guid[0]}`, async (done) => {
+
+            coffee.spawn('node', [pathToExecutable, 'fromRaw', guid[0]])
+                .expect('stdout', guid[1])
+                .expect('code', 0)
+                .end(done)
+        })
+        
+    });
+
+    [
+        [["6C6F59B2D4C5144AB7786A304CCB597F","4630880E6D0B3640AB446C6FB3C44FE3"],"B2596F6C-C5D4-4A14-B778-6A304CCB597F\n0E883046-0B6D-4036-AB44-6C6FB3C44FE3\n".toLowerCase()],
+    ].forEach(guid => {
+        it(`should convert miltiple ${guid[0][0]} ${guid[0][1]} string into ${guid[0][0]} ${guid[0][1]}`, async (done) => {
+
+            coffee.spawn('node', [pathToExecutable, 'fromRaw', guid[0][0], guid[0][1]])
+                .expect('stdout', guid[1])
+                .expect('code', 0)
+                .end(done)
+        })
+        
+    });
+
+    [
+        ["4630880E6D0B3640AB446C6FB3C44FE3\n","0E883046-0B6D-4036-AB44-6C6FB3C44FE3".toLowerCase()],
+        ["6C6F59B2D4C5144AB7786A304CCB597F\n","B2596F6C-C5D4-4A14-B778-6A304CCB597F".toLowerCase()]
+    ].forEach(guid => {
+        it(`should convert ${guid[1]} string into ${guid[0]}`, async (done) => {
+            coffee.spawn('node', [pathToExecutable, 'fromString', guid[1]])
+                .expect('stdout', guid[0])
+                .expect('code', 0)
+                .end(done)
+        })
+        
+    });
+
+    [
+        ["4630880E6D0B3640AB446C6FB3C44FE3\n6C6F59B2D4C5144AB7786A304CCB597F\n",["0E883046-0B6D-4036-AB44-6C6FB3C44FE3".toLowerCase(),"B2596F6C-C5D4-4A14-B778-6A304CCB597F".toLowerCase()]],
+    ].forEach(guid => {
+        it(`should convert miltiple ${guid[1][0]} ${guid[1][1]} string into ${guid[0]}`, async (done) => {
+            coffee.spawn('node', [pathToExecutable, 'fromString', guid[1][0], guid[1][1]])
+                .expect('stdout', guid[0])
+                .expect('code', 0)
+                .end(done)
+        })
+        
+    });
+
+})
