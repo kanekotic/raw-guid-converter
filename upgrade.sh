59e19c8b390e3ff75a44d56fad12f6d7624c3208
kanekotic <alvarojosepl@gmail.com>
2018-07-09 21:33:57 +0200

    fix upgrade script

--- upgrade.sh
+++ upgrade.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -e
+
 yarn upgrade:local 
 yarn test 
 yarn deploy:patch
-#git config --global user.email "travis@travis-ci.org"
-#git config --global user.name "Travis CI"
 
-#git remote add origin-master https://${GH_TOKEN}@github.com/kanekotic/raw-guid-converter.git > /dev/null 2>&1 
-#git push --quiet --set-upstream origin-master master
-#git push --quiet --set-upstream origin-master master --tags
+git config --global user.email "travis@travis-ci.org"
+git config --global user.name "Travis CI"
+
+git remote add origin-master https://${GH_TOKEN}@github.com/kanekotic/raw-guid-converter.git > /dev/null 2>&1 
+git push --quiet --set-upstream origin-master master
+git push --quiet --set-upstream origin-master master --tags
