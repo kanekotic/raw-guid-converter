d07689b0b4e56f7d017e5925efa16cf3fa488bda
kanekotic <alvarojosepl@gmail.com>
2018-07-09 21:16:41 +0200

    updated dependencies

---
+++ upgrade.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+yarn upgrade:local
+yarn test
+yarn deploy:patch
+
+#git config --global user.email "travis@travis-ci.org"
+#git config --global user.name "Travis CI"
+
+#git remote add origin-master https://${GH_TOKEN}@github.com/kanekotic/raw-guid-converter.git > /dev/null 2>&1 
+#git push --quiet --set-upstream origin-master master
+#git push --quiet --set-upstream origin-master master --tags
