c284c99582a341da4261f21377ac26c696cbfd4a
kanekotic <alvarojosepl@gmail.com>
2018-07-09 19:18:03 +0200

    improved code of integration test

--- test/integration/cli-test.js
+++ test/integration/cli-test.js
@@ -1,6 +1,5 @@
 const coffee = require('coffee'),
-    path = require('path'),
-    pathToExecutable = path.join(__dirname, '../../bin/raw-to-guid.js')
+    executable = require.resolve('../../bin/raw-to-guid')
     
 describe('cli', () => {
 
@@ -8,9 +7,9 @@ describe('cli', () => {
         ["4630880E6D0B3640AB446C6FB3C44FE3","0E883046-0B6D-4036-AB44-6C6FB3C44FE3\n".toLowerCase()],
         ["6C6F59B2D4C5144AB7786A304CCB597F","B2596F6C-C5D4-4A14-B778-6A304CCB597F\n".toLowerCase()],
     ].forEach(guid => {
-        it(`should convert ${guid[1]} string into ${guid[0]}`, async (done) => {
-
-            coffee.spawn('node', [pathToExecutable, 'fromRaw', guid[0]])
+        it(`should convert ${guid[1]} string into ${guid[0]}`, done => {
+            console.log(executable)
+            coffee.fork(executable, ['fromRaw', guid[0]])
                 .expect('stdout', guid[1])
                 .expect('code', 0)
                 .end(done)
@@ -21,9 +20,9 @@ describe('cli', () => {
     [
         [["6C6F59B2D4C5144AB7786A304CCB597F","4630880E6D0B3640AB446C6FB3C44FE3"],"B2596F6C-C5D4-4A14-B778-6A304CCB597F\n0E883046-0B6D-4036-AB44-6C6FB3C44FE3\n".toLowerCase()],
     ].forEach(guid => {
-        it(`should convert miltiple ${guid[0][0]} ${guid[0][1]} string into ${guid[0][0]} ${guid[0][1]}`, async (done) => {
+        it(`should convert miltiple ${guid[0][0]} ${guid[0][1]} string into ${guid[0][0]} ${guid[0][1]}`, done => {
 
-            coffee.spawn('node', [pathToExecutable, 'fromRaw', guid[0][0], guid[0][1]])
+            coffee.fork(executable, ['fromRaw', guid[0][0], guid[0][1]])
                 .expect('stdout', guid[1])
                 .expect('code', 0)
                 .end(done)
@@ -35,8 +34,8 @@ describe('cli', () => {
         ["4630880E6D0B3640AB446C6FB3C44FE3\n","0E883046-0B6D-4036-AB44-6C6FB3C44FE3".toLowerCase()],
         ["6C6F59B2D4C5144AB7786A304CCB597F\n","B2596F6C-C5D4-4A14-B778-6A304CCB597F".toLowerCase()]
     ].forEach(guid => {
-        it(`should convert ${guid[1]} string into ${guid[0]}`, async (done) => {
-            coffee.spawn('node', [pathToExecutable, 'fromString', guid[1]])
+        it(`should convert ${guid[1]} string into ${guid[0]}`,  (done) => {
+            coffee.fork(executable, ['fromString', guid[1]])
                 .expect('stdout', guid[0])
                 .expect('code', 0)
                 .end(done)
@@ -47,8 +46,8 @@ describe('cli', () => {
     [
         ["4630880E6D0B3640AB446C6FB3C44FE3\n6C6F59B2D4C5144AB7786A304CCB597F\n",["0E883046-0B6D-4036-AB44-6C6FB3C44FE3".toLowerCase(),"B2596F6C-C5D4-4A14-B778-6A304CCB597F".toLowerCase()]],
     ].forEach(guid => {
-        it(`should convert miltiple ${guid[1][0]} ${guid[1][1]} string into ${guid[0]}`, async (done) => {
-            coffee.spawn('node', [pathToExecutable, 'fromString', guid[1][0], guid[1][1]])
+        it(`should convert miltiple ${guid[1][0]} ${guid[1][1]} string into ${guid[0]}`,  (done) => {
+            coffee.fork(executable, ['fromString', guid[1][0], guid[1][1]])
                 .expect('stdout', guid[0])
                 .expect('code', 0)
                 .end(done)
