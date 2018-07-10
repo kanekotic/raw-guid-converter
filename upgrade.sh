a76b61ccd34b779b571ee26ef8f34d28341ebb15
kanekotic <alvarojosepl@gmail.com>
2018-07-10 08:02:57 +0200

    comment failing step

--- upgrade.sh
+++ upgrade.sh
@@ -11,7 +11,7 @@ git add .
 git commit --allow-empty -m "updated dependencies"
 
 yarn test
-yarn --non-interactive deploy:patch 
+# yarn deploy:patch 
 
 git push --quiet master
 git push --quiet master --tags
