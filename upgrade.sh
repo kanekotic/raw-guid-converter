7c0841ff16a0bb5de95f803331c9d6bda715a8ee
kanekotic <alvarojosepl@gmail.com>
2018-07-10 08:39:15 +0200

    try to attach again to repo [skip ci]

--- upgrade.sh
+++ upgrade.sh
@@ -5,7 +5,7 @@ set -e
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 
-git checkout -b master
+git checkout -b master origin/master
 
 yarn upgrade --latest
 git add .
@@ -14,6 +14,6 @@ git commit --allow-empty -m "updated dependencies"
 yarn test
 # yarn deploy:patch 
 
-git remote add origin-pages https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
-git push --quiet --set-upstream origin-pages master
-git push --quiet --set-upstream origin-pages master --tags
+git remote add origin-master https://${GH_TOKEN}@github.com/MVSE-outreach/resources.git > /dev/null 2>&1
+git push --quiet --set-upstream origin-master master
+git push --quiet --set-upstream origin-master master --tags
