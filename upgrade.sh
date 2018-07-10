2e3a8800e0300234096bbcf11bd64b45f00ecceb
kanekotic <alvarojosepl@gmail.com>
2018-07-10 09:09:51 +0200

    added the deploy step and also [skip ci]

--- upgrade.sh
+++ upgrade.sh
@@ -12,10 +12,10 @@ git checkout -b master-local origin-master/master
 
 yarn upgrade --latest
 git add .
-git commit --allow-empty -m "updated dependencies"
+git commit --allow-empty -m "updated dependencies [skip ci]"
 
 yarn test
-# yarn deploy:patch 
+yarn deploy:patch 
 
 git push --quiet origin-master master-local:master
 git push --quiet origin-master master-local:master --tags
