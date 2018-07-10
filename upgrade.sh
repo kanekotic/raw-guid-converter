038d9fa590d7c2f577fe0e595624fa27997aa70c
kanekotic <alvarojosepl@gmail.com>
2018-07-10 08:22:09 +0200

    change strategy of upgrade to reclone the repo

--- upgrade.sh
+++ upgrade.sh
@@ -2,16 +2,20 @@
 
 set -e
 
+REPO= cut -d "/" -f 2 <<< ${TRAVIS_REPO_SLUG}
+
+git clone https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+cd ${REPO}
+git remote
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
-git config remote.origin.url https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master >/dev/null 2>&1
 
 yarn upgrade --latest
 git add .
 git commit --allow-empty -m "updated dependencies"
 
 yarn test
-# yarn deploy:patch 
+yarn deploy:patch
 
-git push --quiet master HEAD:master
-git push --quiet master HEAD:master --tags
+git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master >/dev/null 2>&1
+git push https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git master --tags >/dev/null 2>&1
