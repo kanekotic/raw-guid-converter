f74ce7aa14d06e42f7bd1f35c2a44319555263ce
kanekotic <alvarojosepl@gmail.com>
2018-07-11 07:20:37 +0200

    try solve tty issue [skip ci]

--- upgrade.sh
+++ upgrade.sh
@@ -15,7 +15,7 @@ git add .
 git commit --allow-empty -m "updated dependencies [skip ci]"
 
 yarn test
-YARN_FORCE_WINPTY=1 yarn version --patch
+yarn deploy:patch
 
 git push --quiet origin-master master-local:master
 git push --quiet origin-master master-local:master --tags
